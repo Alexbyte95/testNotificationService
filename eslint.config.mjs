e823c6d95c93c5db4d1cc466ec859b5ba8a3b390
Alexis Bautista <svedra95@gmail.com>
2025-03-05 22:12:57 -0600

    chore: update project configuration files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,15 +21,18 @@ export default tseslint.config(
       sourceType: 'module',
       parserOptions: {
         projectService: true,
+        // @ts-ignore
         tsconfigRootDir: import.meta.dirname,
       },
     },
   },
   {
     rules: {
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
     },
   },
 );
