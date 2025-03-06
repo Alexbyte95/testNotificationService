9801730276cbc334f629c583a448e2173125bbbb
Alexis Bautista <svedra95@gmail.com>
2025-03-05 22:27:13 -0600

    chore(main): improve main.ts startup logic

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from 'nestjs-pino';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'user.updated',
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      url: '0.0.0.0:50051',
+      package: 'notification',
+      protoPath: join(process.cwd(), 'proto/notification.proto'),
+    },
+  });
+
+  app.useLogger(app.get(Logger));
+
+  await app.startAllMicroservices();
+
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
